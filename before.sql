7dd6d03dcfc35c842ef0cd569c928e4a40716ce7
vzhufk <vzhufk@gmail.com>
2019-05-13 13:03:48 +0300

    [:sparles:] My case Usage.

---
+++ before.sql
@@ -0,0 +1,14 @@
+
+CREATE EXTENSION cube;
+
+
+CREATE EXTENSION "uuid-ossp";
+
+
+CREATE EXTENSION earthdistance;
+
+
+CREATE EXTENSION pg_stat_statements;
+
+
+SELECT pg_catalog.set_config('search_path', '', false);
